ffe0bc089d80b28e1967a1e91ac317c70389f0b9
Markus Pfundstein <markuspfundstein86@gmail.com>
2014-06-25 11:27:28 +0200

    robin indentatie

--- src/objects/behaviour/FleeBehaviour.java
+++ src/objects/behaviour/FleeBehaviour.java
@@ -11,50 +11,52 @@ public class FleeBehaviour extends WanderBehaviour {
 	@Override
 	public boolean execute(Cell c) {
 		
-		if (!(c.properties.currentEnergy > 1)) return false;
-			ArrayList<Tile> perception = c.getPerceptionSet();
-	
-			// first search for other cells in area
-			ArrayList<Tile> dangerousTiles = searchForDanger(perception, c);
-			// if found, move in other direction
-			if (dangerousTiles.size() != 0) {	
-				ArrayList<Tile> options = new ArrayList<Tile>();
-				ArrayList<Tile> tiles = c.getMoveSet();
-				Tile bestTile = null;
-				
-				for (Tile t : dangerousTiles) {
-					Tile proposedTile = null;
-					double distance = 0;
-					for (Tile tile : tiles) {
-							
-						int Dx = tile.x - t.x; 
-						int Dy = tile.y - t.y;
-						double newDistance = Math.sqrt(Dx * Dx + Dy * Dy);
-							
-						if (newDistance > distance) {
-							distance = newDistance;
-							proposedTile = tile;
-						}
+		if (!(c.properties.currentEnergy > 1)) {
+			return false;
+		}
+		ArrayList<Tile> perception = c.getPerceptionSet();
+
+		// first search for other cells in area
+		ArrayList<Tile> dangerousTiles = searchForDanger(perception, c);
+		// if found, move in other direction
+		if (dangerousTiles.size() != 0) {	
+			ArrayList<Tile> options = new ArrayList<Tile>();
+			ArrayList<Tile> tiles = c.getMoveSet();
+			Tile bestTile = null;
+			
+			for (Tile t : dangerousTiles) {
+				Tile proposedTile = null;
+				double distance = 0;
+				for (Tile tile : tiles) {
+						
+					int Dx = tile.x - t.x; 
+					int Dy = tile.y - t.y;
+					double newDistance = Math.sqrt(Dx * Dx + Dy * Dy);
+						
+					if (newDistance > distance) {
+						distance = newDistance;
+						proposedTile = tile;
 					}
-					if(proposedTile != null){
-						options.add(proposedTile);
-					}	
 				}
-				double bestDistance = 0;
-				double totalDistance = 0;
-				for (Tile tile : options) {
-					for(Tile t : dangerousTiles){
-						System.out.println("tile" + tile);
-						System.out.println("dangerous" + t);
-						int Dx = tile.x - t.x; 
-						int Dy = tile.y - t.y;
-						double newDistance = Math.sqrt(Dx * Dx + Dy * Dy);
-						totalDistance = totalDistance + newDistance;
-					}
-					if (totalDistance > bestDistance) {
-						bestDistance = totalDistance;
-						bestTile = tile;
-					}
+				if(proposedTile != null){
+					options.add(proposedTile);
+				}	
+			}
+			double bestDistance = 0;
+			double totalDistance = 0;
+			for (Tile tile : options) {
+				for(Tile t : dangerousTiles) {
+					//System.out.println("tile" + tile);
+					//System.out.println("dangerous" + t);
+					int Dx = tile.x - t.x; 
+					int Dy = tile.y - t.y;
+					double newDistance = Math.sqrt(Dx * Dx + Dy * Dy);
+					totalDistance = totalDistance + newDistance;
+				}
+				if (totalDistance > bestDistance) {
+					bestDistance = totalDistance;
+					bestTile = tile;
+				}
 			}
 			if (bestTile == null) {
 				return false;
@@ -63,8 +65,7 @@ public class FleeBehaviour extends WanderBehaviour {
 			return true;
 		} else { // if not found then no move possible in this behaviour
 			return false;
-		}
-		
+		}	
 	}
 	
 	// looks through perception tiles searching for tiles that have a cell on them
